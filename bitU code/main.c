9e89c0ddc401715b23431ef4462d41aaf8f62859
B0D0 <andrei.bogdan_91@yahoo.com>
2012-12-15 01:55:39 +0200

    Update bitU code/main.c

--- bitU code/main.c
+++ bitU code/main.c
@@ -21,19 +21,21 @@
 //ale mele
 
 #define PI acos(-1)
+#define corectie 15
+#define tcorectie 30
 
 #define ypsus 280
 #define ypjos 420
-#define portar 4
-#define idportar "in4"
+#define portar 10
+#define idportar "in10"
 
 #define minge 0
 #define idminge "in0"
 #define fundas 9
 #define idfundas "in9"
 #define limfundas 325
-#define atacant 7
-#define idatacant "in7"
+#define atacant 10
+#define idatacant "in10"
 #define limatacant 700
 #define xpoarta 116
 #define xfundas 176
@@ -60,6 +62,7 @@ struct robotCoords {
 	int timestamp; /*// timpul cand au fost calculate - UNIXTIME - Ex: 1352460922*/
 }coordrob[13];
 
+struct timeval tv;
 static int run = 1;
 struct mosquitto *mosq;
 int mid;
@@ -82,40 +85,62 @@ void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_
 	coordrob[coordonate->id].y = coordonate->y;
 }
 
-int distanta (int id, int id2)
+float eroare;
+
+void mergi_drept(int id, int putere, int unghi_dorit)
 {
-	int c = coordrob[id].x - coordrob[id2].x;
-	int b = coordrob[id].y - coordrob[id2].y;
-	float dist = b*b + c*c;
-	return dist;
+    eroare= unghi_dorit - coordrob[id].angle;
+
+if((unghi_dorit>90 ) && (unghi_dorit<270))
+{
+	if (eroare<0)
+	{
+		ctr[id].left  = putere + corectie;
+		ctr[id].right = putere - corectie;
+		ctr[id].time   = tcorectie;
+	}
+	else
+	{
+		ctr[id].left  = putere - corectie;
+		ctr[id].right = putere +corectie;
+		ctr[id].time   = tcorectie;
+	}
 }
+else
+{
+	if (eroare<0)
+	{
+		ctr[id].left  = putere - corectie;
+		ctr[id].right = putere + corectie;
+		ctr[id].time   = tcorectie;
+	}
+	else
+	{
+		ctr[id].left  = putere + corectie;
+		ctr[id].right = putere - corectie;
+		ctr[id].time   = tcorectie;
+	}
+}
+}
+int unghiuldorit(int id,int x,int y)
+{
+    double m;
+	m = atan2((double)(y - coordrob[id].y), (double)(x - coordrob[id].x));
+	m = m * 180 / PI;   m = m + 180;
+	printf ("panta: %lf        unghi: %d\n \n \n" , m, coordrob[portar].angle);
+	return m;
+
+}
+
+
 
 int travel (char* idid, int id, int x, int y)
 {
-	//trb vazut cum ne invartim pt in jurul lui 0/360
-	double m;
-	printf ("x: %d " , x);
-	printf ("      y: %d" , y);
-	printf("        coord rob y: %d", coordrob[id].y);
-	printf("coord rob x: %d \n\n\n", coordrob[id].x);
-
-	m = atan2((double)(y - coordrob[id].y), (double)(x - coordrob[id].x));
-	m = m * 180 / PI;    m = m + 180;
-	printf ("panta: %lf \n \n \n" , m);
-	printf ("unghi robot: %lf \n\n", (double)coordrob[id].angle);
 	int c= x - coordrob[id].x;
 	int b= y - coordrob[id].y;
 	float distance = b*b + c*c;
 
-	printf ("diferenta = %d", abs(coordrob[id].angle - m));
-	if (abs(coordrob[id].angle - m) > 30)
-	{
-		ctr[id].right = 50;
-		ctr[id].left = -50;
-		ctr[id].time = 50;
-	}
-	else
-		if (distance < 20*20) {ctr[id].right = 0; ctr[id].left = 0;}
+	if (distance < 20*20) {ctr[id].right = 0; ctr[id].left = 0;}
 		else
 		{
 			ctr[id].right = 80;
@@ -127,154 +152,65 @@ int travel (char* idid, int id, int x, int y)
 	return distance;
 }
 
-void progportar()
-{
-	if (coordrob[portar].angle > 274 && coordrob[portar].angle < 266)
-	{
-		if (coordrob[portar].angle > 274) {ctr[portar].right=50; ctr[portar].left=-50;ctr[portar].time= 60;}
-		else
-			if (coordrob[portar].angle < 266) {ctr[portar].right=-50; ctr[portar].left=50;ctr[portar].time= 60;}
-	}
-	else
-	{if (coordrob[minge].y < ypsus)
-	{
-		if (coordrob[portar].y > ypsus - 5) { ctr[portar].right = -50; ctr[portar].left = -50;ctr[portar].time= 60;}
-	}
-	else
-		if (coordrob[minge].y > ypjos)
-		{
-			if (coordrob[portar].y < ypjos - 5) { ctr[portar].right = 50; ctr[portar].left = 50;ctr[portar].time= 60;}
-		}
-		else
-			if ((coordrob[minge].y < ypjos) && (coordrob[minge].y > ypsus))
-			{if (coordrob[portar].y > coordrob[minge].y)
-			{
-				ctr[portar].right = -50; ctr[portar].left = -50;ctr[portar].time= 60;
-			}
-			if (coordrob[portar].y < coordrob[minge].y)
-			{
-				ctr[portar].right = 50; ctr[portar].left = 50;ctr[portar].time= 60;
-			}
-			}
-	if (coordrob[portar].y > ypjos)
-	{
-		ctr[portar].right = -50;
-		ctr[portar].left = -50;
-		ctr[portar].time= 60;
-	}
-	else
-		if (coordrob[portar].y < ypsus)
-		{
-			ctr[portar].right = 50;
-			ctr[portar].left = 50;
-			ctr[portar].time= 60;
-		}
-	}
-	//mosquitto_loop(mosq, 5, 1);  // mai trb loop daca nu mai dau comanda in functie
+
+void calculate_robot_next_movement() {
+    //Orienteaza-te la un anumit unghi
+    struct robotCoords thisR = coordrob[atacant];
+    double m = unghiuldorit(minge, coordrob[atacant].y,coordrob[atacant].x);
+    // Vreau la 90deg
+    if(thisR.angle> m - 25 && thisR.angle < m + 25) {
+            mergi_drept(atacant,80,m);
+        } else {
+                ctr[atacant].left = 60;
+                ctr[atacant].right = 0;
+                ctr[atacant].time = 50;
+                }
 }
 
-void progfundas()
-{//aranjare fundas
-	if (coordrob[minge].x > limatacant)
-	{
-		if ((coordrob[fundas].x > coordonateinitialefundasx + 5) || (coordrob[fundas].x < coordonateinitialefundasx - 5))
-		{
-			travel(idfundas, fundas,coordonateinitialefundasx,325);
-		}
-		else
-			if (coordrob[fundas].angle <266 || coordrob[fundas].angle > 274)
-			{
-				ctr[fundas].right = 50;
-				ctr[fundas].left = -50;
-			}
-	}
-	//atac minge daca este pe langa fundas
-	else
-	{
-		float distfb;
-		distfb = distanta (fundas,minge);
-		if ((coordrob[fundas].x < coordrob[minge].x) && (distfb < 105*105))
-		{
-			travel(idfundas, fundas,coordrob[minge].x,coordrob[minge].y);
-		}
-		else
-			if (coordrob[fundas].x > coordrob[minge].x)
-			{
-				//atac la minge deja schimbat in spatele mingei, ca apoi sa dea in minge
-				travel(idfundas, fundas,coordrob[minge].x - 15,coordrob[minge].y+30); // depinde unde e mingea..+ cand e sus, - cand e jos
-			}
-			else
-				//sa stea dupa minge
-				if (coordrob[minge].y < ypsusfundas)
-				{
-					if (coordrob[fundas].y > ypsusfundas - 5) { ctr[fundas].right = -50; ctr[fundas].left = -50;}
-				}
-				else
-					if (coordrob[minge].y > ypjosfundas)
-					{
-						if (coordrob[fundas].y < ypjosfundas-5) { ctr[fundas].right = 50; ctr[fundas].left = 50;}
-					}
-					else
-					{
-						if (coordrob[fundas].y > coordrob[minge].y)
-						{
-							ctr[fundas].right = -50; ctr[fundas].left = -50;
-						}
-						if (coordrob[fundas].y < coordrob[minge].y)
-						{
-							ctr[fundas].right = 50; ctr[fundas].left = 50;
-						}
-					}
-	}
+void do_robot_control_loop() {
+    int mid;
+
+    printf("Angle: %d\n", coordrob[atacant].angle);
+    fflush(stdout);
+
+    calculate_robot_next_movement();
+
+    mosquitto_publish(mosq, &mid, idatacant, sizeof(struct control), &ctr[atacant], 0, false);
+    //mosquitto_loop(mosq, 1, 50);
+    gettimeofday(&tv, NULL);
 }
 
-void progatacant()
-{
-	if ((coordrob[minge].x > limfundas) && (coordrob[minge].x < limatacant))
-	{
-		float m,m1,m2;
-		m = (coordrob[minge].y - ypcentrua)/(coordrob[minge].x - xpa);
-		m1 = (coordrob[fundas].y - ypcentrua)/(coordrob[fundas].x - xpa);
-		m2 = (coordrob[atacant].y - ypcentrua)/(coordrob[atacant].x - xpa);
-		m = atan(m);
-		m1 = atan(m1);
-		m2 = atan(m2);
-		if (abs(m1 - m) < abs(m2 - m))
-		{
-			travel(idfundas,fundas,coordrob[minge].x,coordrob[minge].y);
-		}
-		else
-		{
-			travel(idatacant,atacant,coordrob[minge].x,coordrob[minge].y);
-		}
-	}
-	else
-		if (coordrob[minge].x > limatacant)
-		{
-			travel(idatacant, atacant,coordrob[minge].x,coordrob[minge].y);
-		}
-}
+int need_to_send() {
+    struct timeval now;
 
+    gettimeofday(&now, NULL);
+
+    if(tv.tv_usec > 800000)
+        gettimeofday(&tv, NULL);
+
+    if(now.tv_usec >= tv.tv_usec + 100000) {
+            return 1;
+    }
+
+    return 0;
+}
 int main(int argc, char *argv[])
 {
-    struct timeval tv;
-	char clientid[24]="FcBitu'";
+    char clientid[24]="FcBitu'";
 	int rc = 0;
-	time_t result1,result2;
-    gettimeofday(&tv, NULL);
-    result1=tv.tv_usec / 100;
-
 	signal(SIGINT, handle_signal);
 	signal(SIGTERM, handle_signal);
 	mosquitto_lib_init();
 	mosq = mosquitto_new(clientid, true, NULL);
-
+    gettimeofday(&tv,NULL);
+    printf ("%d nirephngep\n\n\n",&tv.tv_usec);
 	if(mosq){
 		mosquitto_connect_callback_set(mosq, connect_callback);
 		mosquitto_message_callback_set(mosq, message_callback);
 		rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
 		mosquitto_subscribe(mosq, NULL, "coords", 0);
 		mosquitto_loop_start(mosq);
+        gettimeofday(&tv,NULL);
 
 		while(run){
 			if(run && rc){
@@ -282,58 +218,15 @@ int main(int argc, char *argv[])
 				printf ("\n run: %d   rc: %d  \n", run,rc);
 				mosquitto_reconnect(mosq);
 			}
-			gettimeofday(&tv, NULL);
-            result2=tv.tv_usec / 100;
-            // incepe softul propriuzis
-			printf("robot: timestamp: %d     X: %d        Y: %d    unghi: %d\n ",coordrob[4].timestamp,coordrob[4] .x,coordrob[4].y,coordrob[4].angle);
-			printf("minge: timestamp: %d     X: %d        Y: %d    unghi: %d\n ",coordrob[0].timestamp,coordrob[0] .x,coordrob[0].y,coordrob[0].angle);
-			progportar();
-			//  progfundas();
-			//  progmijlocasiatacant();
-			//  mosquitto_publish(mosq, &mid, idportar, sizeof(struct control), &ctr[portar], 0, false);
-			//  printf ("comenzi robot: ctr.right: %d  ctr.left: %d timestamp: %d\n", ctr[portar].right,ctr[portar].left,coordrob[0].timestamp);
-        printf("\n\ntimpinitial: %d      timpfinal: %d\n\n", result1, result2);
+			// incepe softul propriuzis
+		    calculate_robot_next_movement();
 
-		if ((result1 > result2) && (result1 > 5000))
-            {
-                int aux;
-                aux = result1;
-                result1 = result2;
-                result2 = aux;
-            }
-            else
-                if (result2 - result1 > 3000)
-                    {
-                    result1 = result2;
-                     mosquitto_publish(mosq, &mid, idportar, sizeof(struct control), &ctr[portar], 0, false);
-                     	printf ("comenzi robot: ctr.right: %d  ctr.left: %d timestamp: %d\n", ctr[portar].right,ctr[portar].left,coordrob[0].timestamp);
-
-                    /*mosquitto_publish(mosq, &mid, idfundas, sizeof(struct control), &ctr[fundas], 0, false);
-                    mosquitto_publish(mosq, &mid, idatacant, sizeof(struct control), &ctr[atacant], 0, false);
-                     */
-                    }
-			/*printf ("comenzi robot: ctr.right: %d  ctr.left: %d timestamp: %d\n", ctr[fundas].right,ctr[fundas].left,coordrob[0].timestamp);
-    printf ("comenzi robot: ctr.right: %d  ctr.left: %d timestamp: %d\n", ctr[atacant].right,ctr[atacant].left,coordrob[0].timestamp);
-			 */
-
-			//travel ("in4", 4,coordrob[minge].x,coordrob[minge].y);
-			//mosquitto_publish(mosq, &mid, "in4", sizeof(struct control), &ctr[4], 0, false);
-			//printf ("comenzi robot: ctr.right: %d  ctr.left: %d timestamp: %d\n", ctr[4].right,ctr[4].left,coordrob[4].timestamp);
-			/*if (xtimp != coordrob[minge].timestamp)
-			{
-				xtimp = coordrob[minge].timestamp;
-				usleep(200000);
-			}
-			else
-			{
-				usleep (150000);
-			}*/
-		//	usleep(200000);
-			fflush(stdout);
-         /*   ctr[portar].right = 0;
-			ctr[portar].left = 0;
-			ctr[portar].time = 10;*/
-		}
+            if(need_to_send())
+                {
+                do_robot_control_loop();
+                }
+		    fflush(stdout);
+        }
 		mosquitto_destroy(mosq);
 	}
 
